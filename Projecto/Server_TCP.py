e3a7aa72ec5231edfe6b6da2d54f36b3a0b3e13a
Francisco <f.sou@hotmail.com>
2018-09-24 14:01:31 +0100

    Novo diretório e TCP Server S e C em python

---
+++ Projecto/Server_TCP.py
@@ -0,0 +1,44 @@
+import socket
+
+ 
+
+msgFromServer       = "Hello TCP Client\n"
+
+bytesToSend         = str.encode(msgFromServer)
+
+serverAddressPort   = ("127.0.0.1", 58002)
+
+bufferSize          = 1024
+
+ 
+
+# Create a TCP socket at client side
+
+TCPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+TCPServerSocket.bind(serverAddressPort)
+
+TCPServerSocket.listen(1)
+
+conection , addr = TCPServerSocket.accept()
+print("Client: ", addr)
+
+msgFromClient = " "
+while (msgFromClient[-1] != '\n'):
+	msgFromClient += bytes.decode(conection.recv(bufferSize))
+
+
+# Send to server using created UDP socket
+nleft = len(bytesToSend)
+while (nleft):
+	nleft -= conection.send(bytesToSend)
+
+ 
+
+
+ 
+
+msg = "Message from Client {}".format(msgFromClient[0])
+conection.close()
+TCPServerSocket.close()
+print(msg)
